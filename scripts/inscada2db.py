7bd65a95a309538424a018d329ac3c1e872032b6
elsea_EDAM <sebastian.gonzalez@suez.com>
2020-02-10 11:31:00 -0300

    script inscada a db

--- scripts/inscada2db.py
+++ scripts/inscada2db.py
@@ -10,6 +10,35 @@ def array2list(array):
 
     return [x[0].value for x in array]
 
+def xl2npMatrix(hoja, minrow, maxrow, mincol, maxcol):
+    '''
+        Toma una hoja de excel y tras definir los limites de extracción de datos,
+        (fila minima, fila máxima, columna minima, columna máxima) devuelve una
+        matriz de numpy.
+
+        :param hoja: Nombre de la hoja de excel de la que extraen los datos
+        :param minrow: Numero de la fila donde se empieza a rescatar los valores.
+        :param maxrow: Numero de la fila donde termina de rescatar los valores.
+        :param mincol: Numero de la columna donde se empieza a rescatar los valores.
+        :param maxcol: Numero de la columna donde termina de rescatar los valores.
+        :returns: Matriz de Numpy con los valores.
+
+        >>> xl2npMatrix('hoja1', 1, 3, 2, 4)
+        array([[1, 0, 1],
+               [1, 0, 1]])
+    '''
+
+    listo = list()
+
+    for row in hoja.iter_rows(min_row = minrow, max_row = maxrow, 
+    min_col = mincol, max_col = maxcol):
+        for cell in row:
+            listo.append(cell.value)
+    
+    listo = np.array(listo)
+    listo = listo.reshape(maxrow - minrow + 1, maxcol - mincol + 1)
+
+    return listo
 
 
 ###########################################################################################
